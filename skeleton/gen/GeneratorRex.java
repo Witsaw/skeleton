474d1f8499400050775f7e3c821bca43e7fbb979
William Chen <z.william.chen@gmail.com>
2018-04-29 19:30:13 -0400

    added 2 generators

---
+++ skeleton/gen/GeneratorRex.java
@@ -0,0 +1,39 @@
+
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+public class GeneratorRex {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException
+	{
+		PrintWriter writer = new PrintWriter("test3.in", "UTF-8");
+		int cases = (int)(Math.random()*5 +1);
+		writer.println(cases);
+		for(int i = 0; i < cases; i++){
+			int n = (int)(Math.random()*100000 +1);
+			int m = (int)(Math.random()*100000 +1);
+			writer.println(n + " " + m);
+			HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+			for(int j = 0; j < m; j++)
+			{
+				
+				int i2 = (int)(Math.random()*n +1);
+				int j2 = (int)(Math.random()*n +1);
+				while(hm.get(i2) != null && hm.get(j2)!= null){
+					if(hm.get(i2) != hm.get(j2))
+					{
+						break;
+					}
+					i2 = (int)(Math.random()*n +1);
+					j2 = (int)(Math.random()*n +1);
+				}
+				hm.put(i2,j2);
+				hm.put(j2, i2);
+				writer.println(i2 + " " + j2);
+			}
+		}
+		writer.close();
+	}
+}
