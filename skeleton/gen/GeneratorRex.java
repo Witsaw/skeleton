0d98c2ca6e6f3397538b75e6bd1ed671ee634cf1
William Chen <z.william.chen@gmail.com>
2018-04-29 21:26:59 -0400

    commit message

--- skeleton/gen/GeneratorRex.java
+++ skeleton/gen/GeneratorRex.java
@@ -1,37 +1,54 @@
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class GeneratorRex {
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException
 	{
-		PrintWriter writer = new PrintWriter("test4.in", "UTF-8");
-		int cases= (int)(Math.random()*5 +1) ;
+		PrintWriter writer = new PrintWriter("004-random.in", "UTF-8");
+		int cases = (int)(Math.random()*5 +1);
 		writer.println(cases);
 		for(int i = 0; i < cases; i++){
 			int n = (int)(Math.random()*100000 +1);
 			int m = (int)(Math.random()*100000 +1);
 			writer.println(n + " " + m);
-			HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+			HashSet<Pair> hm = new HashSet<Pair>();
 			for(int j = 0; j < m; j++)
 			{
 				
 				int i2 = (int)(Math.random()*n +1);
 				int j2 = (int)(Math.random()*n +1);
-				while(hm.get(i2) != null && hm.get(j2)!= null){
-					if(hm.get(i2) != j2 && hm.get(j2) != i2 && i2 != j2)
-					{
-						break;
+				Pair p1= new Pair(i2,j2);
+				Pair p2 = new Pair(j2, i2);
+				while(true){
+					p1 = new Pair(i2,j2);
+					p2 = new Pair(j2, i2);
+					if(i2 != j2){
+						if(!hm.contains(p1) && !hm.contains(p2))
+						{
+							break;
+						}
 					}
 					i2 = (int)(Math.random()*n +1);
 					j2 = (int)(Math.random()*n +1);
 				}
-				hm.put(i2,j2);
-				hm.put(j2, i2);
+				hm.add(p1);
+				hm.add(p2);
 				writer.println(i2 + " " + j2);
 			}
 		}
 		writer.close();
+	}
+}
+class Pair
+{
+	int a, b;
+	Pair(int a1,int b1)
+	{
+		a = a1; 
+		b = b1;
 	}
 }
